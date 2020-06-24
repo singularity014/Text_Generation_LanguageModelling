88e68db11358ec4586a7adfd487dd2a202fd75ee
psharma92 <psharma@naister.com>
2020-06-24 17:46:01 +0200

    text gen with training as well as prediction model with sampling.

---
+++ configs.py
@@ -0,0 +1,10 @@
+# some configuration
+##########################
+MAX_SEQUENCE_LENGTH = 100
+MAX_VOCAB_SIZE = 3000
+EMBEDDING_DIM = 100
+VALIDATION_SPLIT = 0.2
+BATCH_SIZE = 128
+EPOCHS = 2000
+LATENT_DIM = 25
+############################
